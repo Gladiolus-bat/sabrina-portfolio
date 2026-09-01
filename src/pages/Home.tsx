02ed30240bd08203caa11bd75b0c37198e7dab13
Sabrina Pradhan <pradhansabrina32@gmail.com>
2026-09-01 20:17:06 +0545

    feat: implement Home component with Navbar and Footer

---
+++ src/pages/Home.tsx
@@ -0,0 +1,16 @@
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+
+export default function Home() {
+    return (
+        <div className="bg-[#FAF8F5] text-[#4A5759] font-sans selection:bg-[#EDAFB8]/40 selection:text-[#4A5759] min-h-screen">
+            <Navbar />
+
+            <main className="flex-grow">
+                
+            </main>
+            
+            <Footer />
+        </div>
+    )
+}
