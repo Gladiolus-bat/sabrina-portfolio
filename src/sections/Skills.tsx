964e94df2ae3a0d317c504bf8220c65d32eb0f36
Sabrina Pradhan <pradhansabrina32@gmail.com>
2026-09-02 08:36:13 +0545

    feat: add initial skills data structure for frontend technologies

---
+++ src/sections/Skills.tsx
@@ -0,0 +1,7 @@
+const skills = [
+    { name: 'React', category: 'Frontend', bg: '#B0C4B1'},
+    { name: 'Next.js', category: 'Fullstack', bg: '#DEDBD2'},
+    { name: 'Tailwind CSS', category: 'Styling', bg: '#EDAFB8'},
+    { name: 'JavaScript', category: 'Backend', bg: '#B0C4B1'},
+];
+
