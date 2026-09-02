cf4f9a2ec0d72349aebe8d0e0e179f3d62de90e2
Sabrina Pradhan <pradhansabrina32@gmail.com>
2026-09-02 19:39:31 +0545

    feat: add Experience interface and initial experiences data

---
+++ src/data/experience.ts
@@ -0,0 +1,37 @@
+export interface Experience {
+    title: string;
+    startYear: string;
+    endYear: string;
+    company: string;
+    point1: string;
+    point2: string;
+    point3: string;
+}
+
+export const experiencesData: Experience[] = [
+    {
+        title: '',
+        startYear: '',
+        endYear: '',
+        company: '',
+        point1: '',
+        point2: '',
+        point3: '',
+    }, {
+        title: '',
+        startYear: '',
+        endYear: '',
+        company: '',
+        point1: '',
+        point2: '',
+        point3: '',
+    }, {
+        title: '',
+        startYear: '',
+        endYear: '',
+        company: '',
+        point1: '',
+        point2: '',
+        point3: '',
+    }
+];
